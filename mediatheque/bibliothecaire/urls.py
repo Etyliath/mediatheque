7cb2e6f236458c7f2d2a0714db6d2a588127b70f
Edouard DA SILVA <esdasilva@hotmail.fr>
2024-04-28 17:40:06 +0200

    create views and form for models Book, Dvd,Cd,Boardgame Issue #5

--- mediatheque/bibliothecaire/urls.py
+++ mediatheque/bibliothecaire/urls.py
@@ -7,7 +7,14 @@ from . import views
 urlpatterns = [
     path("", views.listing, name="listing"),
     path("<int:book_id>" , views.detail_book, name="detail-book"),
+    path("member/<int:member_id>", views.member_delete, name="member-delete"),
+    path("member/", views.member_listing, name="member-listing"),
+    
+    path("member/add/", views.member_create, name="member-create"),
     path("book/add/", views.book_create, name="book-create"),
+    path("dvd/add/", views.dvd_create, name="dvd-create"),
+    path("cd/add/", views.cd_create, name="cd-create"),
+    path("boardgame/add/", views.boardgame_create, name="boardgame-create"),
     path("empruntbook/add/", views.emprunt_book_create, name="emprunt-book-create")
     # path("<int:id>", views.detail, name="detail"),
 
