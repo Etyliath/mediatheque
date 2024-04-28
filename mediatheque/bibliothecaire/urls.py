a1a65db26b7f09b89b2ff3400caf3e5fe024fee9
Edouard DA SILVA <esdasilva@hotmail.fr>
2024-04-28 15:04:54 +0200

    app bibliothecaire create diplay index page Issue #1

---
+++ mediatheque/bibliothecaire/urls.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    # path("", views.listing, name="listing"),
+    # path("<int:book_id>" , views.detail_book, name="detail-book"),
+    # path("book/add/", views.book_create, name="book-create"),
+    # path("empruntbook/add/", views.emprunt_book_create, name="emprunt-book-create")
+    # path("<int:id>", views.detail, name="detail"),
+
+]
