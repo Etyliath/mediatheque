594d05b2e7f304d4fb409175262b9777d253cd3a
Edouard DA SILVA <esdasilva@hotmail.fr>
2024-04-30 23:05:10 +0200

    all features ok Issue #8

--- mediatheque/mediatheque/urls.py
+++ mediatheque/mediatheque/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from bibliothecaire import views
+from django.conf import settings
 
 urlpatterns = [
     path("", views.index, name='index'),
